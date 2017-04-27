b5bed0797f1eb6f17a8af3f996c399b3612f22e8
danekolak <danijel.kolak123@hotmail.com>
2017-04-27 10:21:12 +0200

    hello

--- MyCodeCamp/Controllers/CampsController.cs
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,11 +12,13 @@ namespace MyCodeCamp.Controllers
     [Route("api/camps")]
     public class CampsController : Controller
     {
+        private readonly ILogger<CampsController> _logger;
         private readonly ICampRepository _repo;
 
-        public CampsController(ICampRepository repo)
+        public CampsController(ICampRepository repo, ILogger<CampsController> logger)
         {
             _repo = repo;
+            _logger = logger;
         }
 
         [HttpGet("")]
@@ -23,7 +29,7 @@ namespace MyCodeCamp.Controllers
             return Ok(camps);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "CampGet")]
         public IActionResult Get(int id, bool includeSpeakers = false)
         {
             try
@@ -41,6 +47,51 @@ namespace MyCodeCamp.Controllers
             return BadRequest();
         }
 
-        private int n = 3;
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Camp model)
+        {
+            try
+            {
+                _logger.LogInformation("Creating a new Code Camp");
+
+
+                _repo.Add(model);
+                if (await _repo.SaveAllAsync())
+                {
+                    var newUri = Url.Link("CampGet", new { id = model.Id });
+                    return Created(newUri, model);
+                }
+                // _logger.LogWarning("Could not save Camp to the database");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Threw exception while saving Camp: {ex}");
+            }
+            return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Camp model)
+        {
+            try
+            {
+                var oldCamp = _repo.GetCamp(id);
+                if (oldCamp == null)
+                    return NotFound($"Could not find a camp with an ID of {id}");
+
+                oldCamp.Name = model.Name ?? oldCamp.Name;
+                oldCamp.Description = model.Description ?? oldCamp.Description;
+                oldCamp.Location = model.Location ?? oldCamp.Location;
+                oldCamp.Length = model.Length > 0 ? model.Length : oldCamp.Length;
+                oldCamp.EventDate = model.EventDate != DateTime.MinValue ? model.EventDate : oldCamp.EventDate;
+
+                if (await _repo.SaveAllAsync())
+                    return Ok(oldCamp);
+            }
+            catch (Exception)
+            {
+            }
+            return BadRequest("Couldn't update Camp");
+        }
     }
 }
--- MyCodeCamp/Startup.cs
+++ MyCodeCamp/Startup.cs
@@ -41,7 +41,8 @@ namespace MyCodeCamp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
             CampDbInitializer seeder)
         {
             loggerFactory.AddConsole(_config.GetSection("Logging"));
